e0f88739c1afdde29d04514fb4ebd07d0bf7e55f
charaf marghin <developermarghin@gmail.com>
2023-03-23 23:45:55 +0000

    add Header component to layout

---
+++ app/components/Header.tsx
@@ -0,0 +1,31 @@
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
+function Header(): JSX.Element {
+  return (
+    <header className="max-w-[90rem] mx-auto">
+      <div className="grid grid-cols-3 p-10 items-center">
+        <Bars3Icon className="h-8 w-8 cursor-pointer" />
+
+        <Link
+          className="text-center text-text_important_black"
+          href="/"
+          prefetch={false}
+        >
+          {/* gradient background text */}
+          <p className="text-4xl font-serif bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-orange-700">
+            Next News
+          </p>
+        </Link>
+
+        <div className="flex items-center justify-end space-x-2">
+          <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800">
+            Subscribe Now
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
